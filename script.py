eb664ff464b706bccf2187d1d86c7f55e4508a90
Rodrigo Santos <rodrigom376@gmail.com>
2024-05-21 23:40:07 -0300

    tratando erro de números inexistentes

--- script.py
+++ script.py
@@ -19,15 +19,20 @@ elemento_pesquisa = navegador.find_element(By.XPATH, "//p[@class='selectable-tex
 for i, mensagem in enumerate(contatos_df['Mensagem']):
     pessoa = contatos_df.loc[i, "Pessoa"]
     numero = contatos_df.loc[i, "Número"]
-    texto = urllib.parse.quote(f"Oi {pessoa}! {mensagem}")
+    texto = urllib.parse.quote(f"{mensagem}")
     link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
     navegador.get(link)
-    time.sleep(20)
-    in_mensagem = navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
-    in_mensagem.send_keys(Keys.ENTER)
-    time.sleep(3)
-    print("Texto enviado")
+    time.sleep(30)
+    try:
+        in_mensagem = navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
+        in_mensagem.send_keys(Keys.ENTER)
+        time.sleep(2)
+        print("Texto enviado")
+    except Exception as e:
+        print(f"Erro ao enviar mensagem para {numero}: {str(e)}")
+        continue 
 print("Todas as mensagens enviadas!")
 
 while True:
     pass
+
